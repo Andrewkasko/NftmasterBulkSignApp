ff28411233146be2cac4ca140e385272190d99f8
Andrew Kaskaniotis <andrewkaskaniotis@Andrews-MBP.modem>
2022-12-28 16:38:02 +1100

    V 2.0.1 - Compatibility for copying and pasting within application

--- src/index.js
+++ src/index.js
@@ -51,16 +51,33 @@ app.on('window-all-closed', () => {
 });
 
 
-const menu = [
-  {
-    label: 'File',
-    submenu :[{
-      label: 'Quit',
-      click : () => app.quit(),
-      accelerator: 'CmdOrCtrl+W'
-    }]
-  }
-]
+var menu = [{
+  label: "Application",
+  submenu: [
+      { label: "Quit", accelerator: "Command+Q", click: function() { app.quit(); }}
+  ]}, {
+  label: "Edit",
+  submenu: [
+      { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+      { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+      { type: "separator" },
+      { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+      { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+      { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+      { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+  ]}
+];
+
+// const menu = [
+//   {
+//     label: 'File',
+//     submenu :[{
+//       label: 'Quit',
+//       click : () => app.quit(),
+//       accelerator: 'CmdOrCtrl+W'
+//     }]
+//   }
+// ]
 
 
 
